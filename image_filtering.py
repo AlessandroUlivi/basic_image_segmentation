b718044fb45e29cdc7497e583411cbf86b675fec
Alessandro Ulivi <ale.ulivi@gmail.com>
2024-07-01 17:12:33 +0200

    initialize highpass_area_filter in image_filtering.py

--- image_filtering.py
+++ image_filtering.py
@@ -3,6 +3,7 @@ from scipy.signal import convolve
 from skimage.filters import median as medianfilter
 from skimage.filters import frangi
 from skimage.util import img_as_float32
+from skimage.measure import label, regionprops
 import cv2
 
 
@@ -61,3 +62,58 @@ def frangi_filter(immg, **kwargs):
 
     return frangi(immg_copy, **kwargs)
 
+
+def highpass_area_filter(input__binary__imag_e, area_highpass_thr, return_area_list=False, input_is_label_image=False):
+    """
+    Given a binary input array and a threshold for area (the threshold is in number of pixels), it filters the structures of the binary image and only keep those whose area is
+    higher than the threshold.
+    
+    By default input__binary__imag_e must be a binary mask. It is however possible to alternatively provide a label image.
+    If a label image is provided for input__binary__imag_e, the parameter input_is_label_image must be set to True.
+
+    NOTE: the option of providing input__binary__imag_e as a label image hasn't been properly tested.
+    """
+    #Copy the input image
+    input__binary__imag_e_copy = input__binary__imag_e.copy()
+
+    # label image regions if input image is not already a label image
+    if input_is_label_image:
+        label__im_g = input__binary__imag_e.copy()
+    else:
+        label__im_g = label(input__binary__imag_e_copy)
+    
+    #measure the properties of the region
+    label__im_g_properties = regionprops(label__im_g)
+
+    #Initialize a zero array to be modified as output array
+    out_put_arr_ay = np.zeros((input__binary__imag_e_copy.shape[0], input__binary__imag_e_copy.shape[1]))
+
+    #Initialize a collection list for the areas
+    areas_cl = []
+    
+    #Iterate through the regions of the labelled image, identified using measure.regionprops
+    for re_gi_on in label__im_g_properties:
+        
+        #Get the area of the region
+        re_gion_area = re_gi_on.area
+
+        #Add area to collection list
+        areas_cl.append(re_gion_area)
+
+        #If the region area is higher than the highpass area threshold, modify the output array
+        if re_gion_area >= area_highpass_thr:
+    
+            #Get region coordinates
+            re_gi_on_coordinates = re_gi_on.coords
+    
+            #Unzip the coordinates in individual lists
+            unzipped_re_gi_on_coordinates = [list(t) for t in zip(*re_gi_on_coordinates)]
+            
+            #Set output array values at region coordinates to 255
+            out_put_arr_ay[unzipped_re_gi_on_coordinates[0], unzipped_re_gi_on_coordinates[1]] = 1
+    
+    #Return output array and area list if return_area_list is selected, else only return the output array
+    if return_area_list:
+        return out_put_arr_ay, areas_cl
+    else:
+        return out_put_arr_ay
